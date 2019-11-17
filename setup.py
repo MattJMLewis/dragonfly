7199da6567fa018cbfcb3b9a96578dd0a1bfd027
MattJMLewis <MattJMLewis@users.noreply.github.com>
2019-11-17 18:15:32 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ import setuptools
 
 setuptools.setup(
     name="dragonfly-web",
-    version="0.0.3",
+    version="0.0.3a",
     author="Matthew Lewis",
     description="A web framework.",
     long_description="A web framework that is loosely based on the syntax of Django and Laravel. It is inteded to be "
                        "used as a rapid development tool.",
     url="https://github.com/MattJMLewis/dragonfly",
-    packages=['dragonfly'],
+    packages=setuptools.find_packages(),
     classifiers=["Programming Language :: Python :: 3.7"]
 )
